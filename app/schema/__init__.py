5d62defa895f7c2d3c58336ff7359d16a79b095e
neerajskumar <neerajindeevaram12@gmail.com>
2022-05-09 03:06:57 +0530

    Updated - Schema

--- app/schema/__init__.py
+++ app/schema/__init__.py
@@ -1,3 +1,4 @@
 from app.schema.raspberry_module import RaspberryModuleAPIs
 from app.schema.mobile_client import MobileClientAPIs
-from app.schema.web_client import WebClientAPIs
+from app.schema.img_frame import ImageFrame
+# from app.schema.web_client import WebClientAPIs
