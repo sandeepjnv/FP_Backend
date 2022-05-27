18e03fea9806730bbbb6a827a03c384e63636542
sandeepjnv <sandeepsatheesanjnv@gmail.com>
2022-05-27 17:40:14 +0530

    Updated - Imports

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -3,4 +3,5 @@ from flask_restplus import Api
 from app.api.raspberry_module import ImageFrame
 from app.api.raspberry_module import LocationUpdate
 
-from app.api.mobile_client import ListAllvehicles
+from app.api.vehicle import ListAllVehicles
+from app.api.users import Users
