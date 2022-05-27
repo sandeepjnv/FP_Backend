7d605bcc5dfa0e5ed7d8e1ab626d454c1a087d30
sandeepjnv <sandeepsatheesanjnv@gmail.com>
2022-05-27 18:30:26 +0530

    Removed - files

--- app/schema/mobile_client.py
+++
@@ -1,9 +0,0 @@
-from flask_restplus import Namespace, fields
-
-class MobileClientAPIs:
-    ins_namespace = Namespace('Mobile Client APIs')
-
-    ins_mob_login_request = ins_namespace.model('Mobile client',{
-        'str_user_name': fields.String(required=True, description='Username'),
-        'str_password': fields.String(required=True, description='Password'),
-    })
