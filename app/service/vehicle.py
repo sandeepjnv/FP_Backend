84698ea09e52c55f02ed19b61b6aeed1221bfb5f
sandeepjnv <sandeepsatheesanjnv@gmail.com>
2022-05-31 07:47:51 +0530

    Updated - Search,List buses with source,destination

--- app/service/vehicle.py
+++ app/service/vehicle.py
@@ -4,6 +4,8 @@ import json
 
 def get_all_buses(request):
     dct_request = request.json
+    source_lat , source_long = dct_request.get("str_source").split(",")
+    dest_lat , dest_long = dct_request.get("str_destination").split(",")
     try:
         ins_buses = Vehicle.query.all()
         dct_buses = {}
@@ -12,8 +14,29 @@ def get_all_buses(request):
             del dct_bus["_sa_instance_state"]
 
             # check for buses between routes
+            intDirection = dct_bus["int_direction"]
+            if int_direction == 0:
+                arrRoute = dct_bus["json_route_map"].reverse()
+            else:
+                arrRoute = dct_bus["json_route_map"]
 
-            dct_buses[ins_bus.pk_bint_vehicle_id] = dct_bus
+            blnSource = False
+            blnDestination = False
+            for stop in arrRoute:
+                if blnSource and blnDestination:
+                    # stop looping stops ,skip current bus and check next bus                    
+                    break
+                else:
+                    stop_lat,stop_long = stop.split(",")
+
+                    if not blnSource:
+                        if source_lat == stop_lat and source_long == stop_long:
+                            # source matched look for dest
+                            blnSource = True
+                    else:
+                        if dest_lat == stop_lat and dest_long == stop_long:
+                            blnDestination = True
+                            dct_buses[ins_bus.pk_bint_vehicle_id] = dct_bus
         
         if dct_buses:
             return dct_buses, 201    
